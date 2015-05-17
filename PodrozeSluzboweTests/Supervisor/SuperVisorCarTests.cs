27e4d4a94c19a219ad718f68e7ab9611f1db308e
mastertylus <mastertylus@gmail.com>
2015-05-17 11:56:12 +0200

    tests

---
+++ PodrozeSluzboweTests/Supervisor/SuperVisorCarTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PodrozeSluzbowe.Supervisor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PodrozeSluzbowe.Supervisor.Tests
+{
+    [TestClass()]
+    public class SuperVisorCarTests
+    {
+        [TestMethod()]
+        public void checkNumberOfSeatsValueTest()
+        {
+
+
+
+            Assert.Fail();
+        }
+    }
+}
